0e8923dbc915ec00d109b83d8efd5bcb2886b3b7
Eric Chiang <eric@oblique.security>
2025-07-28 21:07:11 -0700

    add bash script for printing the jwt claims

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+curl -s \
+	-H "Authorization: Bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" \
+	"$ACTIONS_ID_TOKEN_REQUEST_URL?audience=oblique" | \
+	jq '.value' | \
+	awk -F'.' '{print $2}' | \
+	tr '_-' '/+' | \
+	base64 -d | \
+	jq .
+
+
